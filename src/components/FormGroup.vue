cf2f456716f24a8f085174824eaf4828c9cc601e
unknown <renzivanyapenguio@gmail.com>
2020-09-06 02:11:31 +0800

    added solution for the test

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="form-group">
+        <label for="input-value" v-show="label">{{ label }}</label>
+        <input id="input-value" type="text" placeholder="Value" v-model="inputValue" @input="onChange(inputValue)">
+        <button @click="onSubmit">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: null,
+            required: false
+        },
+        value: {
+            type: String,
+            default: '',
+            required: false
+        }
+    },
+    data () {
+        return {
+            inputValue: this.value
+        }
+    },
+    methods: {
+        onChange (val) {
+            this.$emit('valueChange', val)
+        },
+        onSubmit () {
+            this.$emit('onSubmit', this.inputValue)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home-page">
+      <form @submit.prevent="">
+        <FormGroup
+            :label="label"
+            :value="value"
+            @valueChange="valueChange"
+            @onSubmit="onSubmit"
+        />
+      </form>
+  </div>
+</template>
+
+<script>
+import FormGroup from './FormGroup'
+
+export default {
+    name: 'HomePage',
+    components: {
+        FormGroup
+    },
+    data () {
+        return {
+            label: '',
+            value: '',
+        }
+    },
+    methods: {
+        valueChange (val) {
+            this.value = val
+        },
+        onSubmit (val) {
+            alert(`Value: ${val}`)
+        }
+    }
+}
+</script>
